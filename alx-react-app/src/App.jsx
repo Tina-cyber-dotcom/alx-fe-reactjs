ff1347c743ef519a4633db11de55f38e1f872bb6
Christine <tinarosemoraa@gmail.com>
2025-09-06 23:01:04 +0300

    Intergrate Header, MainContent, and Footer components into App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
-function App() {
+export default function App() {
   return (
     <>
       <Header />
@@ -14,4 +12,3 @@ function App() {
   );
 }
 
-export default App;
