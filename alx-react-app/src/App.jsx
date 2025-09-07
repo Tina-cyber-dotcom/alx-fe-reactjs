9d8be7f377a704597493bfd9589f1e7d8f3d9ac2
Christine <tinarosemoraa@gmail.com>
2025-09-07 22:21:29 +0300

    Add UserProfile component and intergrate into app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,23 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import React from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
-export default function App() {
+function App() {
   return (
-    <>
+    <div>
       <Header />
       <MainContent />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
       <Footer />
-    </>
+    </div>
   );
 }
+
+export default App;
 
