951f3a4163e6c7a06ac2df00973ff1dc99472163
Christine <tinarosemoraa@gmail.com>
2025-09-24 14:00:06 +0300

    Add updated App.jsx with Search component and theme styles withTailwind

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  const res = await axios.get(`https://api.github.com/users/${username}`);
-  return res.data;
+export const fetchUserData = async (username, location = "", minRepos = 0) => {
+  try {
+    let query = `${username}`;
+    if (location) query += `+location:${location}`;
+    if (minRepos) query += `+repos:>=${minRepos}`;
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${query}`);
+    return res.data.items;
+  } catch (error) {
+    console.error("Error fetching GitHub users:", error);
+    throw error;
+  }
 };
