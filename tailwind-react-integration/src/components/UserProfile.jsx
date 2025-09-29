10e398e2909569e530944b28d3a9fd4b2445affc
Christine <tinarosemoraa@gmail.com>
2025-09-29 15:54:36 +0300

    Add UserPofile with advanced interactions and transitions with Tailwind Css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,14 +1,16 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center transition-shadow duration-300 ease-in-out hover:shadow-xl">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 "
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">
+        John Doe
+      </h1>
       <p className="text-gray-600 text-sm">
-        Developer at Example Co. Loves to write code and explore new
+        Developer at Tinah's Tech Company. Loves to write code and explore new
         technologies.
       </p>
     </div>
@@ -16,4 +18,3 @@ function UserProfile() {
 }
 
 export default UserProfile;
-
