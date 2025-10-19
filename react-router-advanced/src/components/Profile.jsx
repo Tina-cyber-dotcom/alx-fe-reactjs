a2cc0e47fe3ec4c0be225afcc5e0d8f869417649
Christine <tinarosemoraa@gmail.com>
2025-10-19 22:48:24 +0300

    Add Advanced Routing in React with React Router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,23 @@
-import { Outlet, Link } from "react-router-dom";
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 export default function Profile() {
   return (
     <div>
-      <h1>Profile Page</h1>
+      <h2>User Profile</h2>
+      <p>Welcome to your profile dashboard.</p>
+
       <nav>
         <Link to="details">Profile Details</Link> |{" "}
         <Link to="settings">Profile Settings</Link>
       </nav>
-      <Outlet />
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 }
