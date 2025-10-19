7e6faa660aa0c69125474628f7f121524d8adfbb
Christine <tinarosemoraa@gmail.com>
2025-10-19 22:52:24 +0300

    Add Advanced Routing in React with React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,26 +1,20 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
-import BlogPost from "./pages/BlogPost";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Blog from "./components/Blog";         
+import BlogPost from "./components/BlogPost"; 
+import Profile from "./components/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <nav style={{ marginBottom: "1rem" }}>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/posts/1">Blog Post 1</Link>
-      </nav>
-
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+
+        <Route path="/blog" element={<Blog />} />
+
+        <Route path="/blog/:id" element={<BlogPost />} />
 
         <Route
           path="/profile/*"
@@ -29,14 +23,10 @@ export default function App() {
               <Profile />
             </ProtectedRoute>
           }
-        >
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-
-        <Route path="/posts/:postId" element={<BlogPost />} />
+        />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
+export default App;
